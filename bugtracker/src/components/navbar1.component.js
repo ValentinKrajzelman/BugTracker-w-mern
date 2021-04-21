78a5331ee2dda77a5b98acf78f3821b52015792d
valentin krajzelman <valentinkrajzelman@hotmail.com>
2021-04-21 19:22:37 -0300

    added the dropdown menu for projects

--- bugtracker/src/components/navbar1.component.js
+++ bugtracker/src/components/navbar1.component.js
@@ -5,7 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 // import LoginButton from "./loginBtn.component";
 
 const NavBar1 = () => {
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
   return (
     <nav className="navbar bg-light navbar-expand-sm navbar-light">
@@ -13,10 +13,37 @@ const NavBar1 = () => {
         BugTracker <small>v0.1</small>
       </Link>
 
+      <div className="nav-item dropdown navbar-text">
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbardrop"
+          data-toggle="dropdown"
+        >
+          Lista De Proyectos
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">
+            Link 1
+          </a>
+          <a class="dropdown-item" href="#">
+            Link 2
+          </a>
+          <a class="dropdown-item" href="#">
+            Link 3
+          </a>
+        </div>
+      </div>
+
       <ul className="navbar-nav ml-auto">
         {!isAuthenticated && (
           <li className="nav-item">
             <button onClick={() => loginWithRedirect()}>Log In</button>
+          </li>
+        )}
+        {isAuthenticated && (
+          <li className="nav-item mr-3">
+            <div className="navbar-text">{user.name}</div>
           </li>
         )}
         {isAuthenticated && (
